d2a48eb45b6d4e339e0092ccf57edd0842356474
travis@gockelhut.com <travis@gockelhut.com>
2012-02-18 11:22:29 -0700

    Adds a forwarding header.  Probably not needed...but whatever.

---
+++ include/json-voorhees/forward.hpp
@@ -0,0 +1,21 @@
+/** \file json-voorhees/forward.hpp
+ *  
+ *  Copyright (c) 2012 by Travis Gockel. All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify it under the terms of the Apache License
+ *  as published by the Apache Software Foundation, either version 2 of the License, or (at your option) any later
+ *  version.
+ *
+ *  \author Travis Gockel (travis@gockelhut.com)
+**/
+#ifndef __JSON_VOORHEES_FORWARD_HPP_INCLUDED__
+#define __JSON_VOORHEES_FORWARD_HPP_INCLUDED__
+
+namespace jsonv
+{
+
+class value;
+
+}
+
+#endif/*__JSON_VOORHEES_FORWARD_HPP_INCLUDED__*/
