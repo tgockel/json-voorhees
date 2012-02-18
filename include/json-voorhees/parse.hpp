d45b2bb94e62ba266182d3b2b322fd450a46bbb0
travis@gockelhut.com <travis@gockelhut.com>
2012-02-18 11:21:52 -0700

    Adds copyright info.  Apache sounds good to me.

--- include/json-voorhees/parse.hpp
+++ include/json-voorhees/parse.hpp
@@ -1,3 +1,13 @@
+/** \file json-voorhees/parse.hpp
+ *  
+ *  Copyright (c) 2012 by Travis Gockel. All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify it under the terms of the Apache License
+ *  as published by the Apache Software Foundation, either version 2 of the License, or (at your option) any later
+ *  version.
+ *
+ *  \author Travis Gockel (travis@gockelhut.com)
+**/
 #ifndef __JSON_VOORHEES_PARSE_HPP_INCLUDED__
 #define __JSON_VOORHEES_PARSE_HPP_INCLUDED__
 
--- src/json-voorhees/parse.cpp
+++ src/json-voorhees/parse.cpp
@@ -1,3 +1,13 @@
+/** \file
+ *  
+ *  Copyright (c) 2012 by Travis Gockel. All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify it under the terms of the Apache License
+ *  as published by the Apache Software Foundation, either version 2 of the License, or (at your option) any later
+ *  version.
+ *
+ *  \author Travis Gockel (travis@gockelhut.com)
+**/
 #include <json-voorhees/parse.hpp>
 
 #include "char_convert.hpp"
