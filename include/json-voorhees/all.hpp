75b413c131e8b2dc05a98974f80cbdc064b567f3
travis@gockelhut.com <travis@gockelhut.com>
2012-02-20 01:06:31 -0700

    Adds an include all header.

---
+++ include/json-voorhees/all.hpp
@@ -0,0 +1,21 @@
+/** \file json-voorhees/all.hpp
+ *  A head which includes all other JSON Voorhees headers.
+ *  
+ *  Copyright (c) 2012 by Travis Gockel. All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify it under the terms of the Apache License
+ *  as published by the Apache Software Foundation, either version 2 of the License, or (at your option) any later
+ *  version.
+ *
+ *  \author Travis Gockel (travis@gockelhut.com)
+**/
+#ifndef __JSON_VOORHEES_ALL_HPP_INCLUDED__
+#define __JSON_VOORHEES_ALL_HPP_INCLUDED__
+
+#include "forward.hpp"
+#include "object.hpp"
+#include "parse.hpp"
+#include "standard.hpp"
+#include "value.hpp"
+
+#endif/*__JSON_VOORHEES_ALL_HPP_INCLUDED__*/
