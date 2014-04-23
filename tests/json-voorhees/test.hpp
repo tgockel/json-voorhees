94f03c525a852e0e31acd8f19a1b4a9ed230926a
Travis Gockel <travis@gockelhut.com>
2014-04-22 18:00:42 -0600

    Adds jsonv_test::unit_test::name.

--- tests/json-voorhees/test.hpp
+++ tests/json-voorhees/test.hpp
@@ -1,6 +1,6 @@
 /** \file
  *  
- *  Copyright (c) 2012 by Travis Gockel. All rights reserved.
+ *  Copyright (c) 2012-2014 by Travis Gockel. All rights reserved.
  *
  *  This program is free software: you can redistribute it and/or modify it under the terms of the Apache License
  *  as published by the Apache Software Foundation, either version 2 of the License, or (at your option) any later
@@ -71,6 +71,11 @@ public:
     explicit unit_test(const std::string& name);
     
     bool run();
+    
+    const std::string& name() const
+    {
+        return _name;
+    }
     
 private:
     virtual void run_impl() = 0;
