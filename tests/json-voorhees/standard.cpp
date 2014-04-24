5526937ff7b773a2c6c9b85b66dbc751ac9fae2d
Travis Gockel <travis@gockelhut.com>
2014-04-23 20:58:01 -0600

    Adds a test for the is_consistent function.

---
+++ tests/json-voorhees/standard.cpp
@@ -0,0 +1,18 @@
+/** \file
+ *  
+ *  Copyright (c) 2014 by Travis Gockel. All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify it under the terms of the Apache License
+ *  as published by the Apache Software Foundation, either version 2 of the License, or (at your option) any later
+ *  version.
+ *
+ *  \author Travis Gockel (travis@gockelhut.com)
+**/
+#include "test.hpp"
+
+#include <json-voorhees/standard.hpp>
+
+TEST(standard_is_consistent)
+{
+    ensure(jsonv::is_consistent());
+}
