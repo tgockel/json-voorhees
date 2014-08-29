9e33c993475dab138a82b6c8c276630be17f3eee
Travis Gockel <travis@gockelhut.com>
2014-08-29 15:31:37 -0600

    Adds Doxygen comments to encoder.

--- include/jsonv/encode.hpp
+++ include/jsonv/encode.hpp
@@ -32,35 +32,118 @@ public:
     void encode(const jsonv::value& source);
     
 protected:
+    /** Write the null value.
+     *  
+     *  \code
+     *  null
+     *  \endcode
+    **/
     virtual void write_null() = 0;
     
+    /** Write the opening of an object value.
+     *  
+     *  \code
+     *  {
+     *  \endcode
+    **/
     virtual void write_object_begin() = 0;
     
+    /** Write the closing of an object value.
+     *  
+     *  \code
+     *  }
+     *  \endcode
+    **/
     virtual void write_object_end() = 0;
     
+    /** Write the key for an object, including the separator.
+     *  
+     *  \code
+     *  "key":
+     *  \endcode
+    **/
     virtual void write_object_key(string_ref key) = 0;
     
+    /** Write the delimiter between two entries in an object.
+     *  
+     *  \code
+     *  ,
+     *  \endcode
+    **/
     virtual void write_object_delimiter() = 0;
     
+    /** Write the opening of an array value.
+     *  
+     *  \code
+     *  [
+     *  \endcode
+    **/
     virtual void write_array_begin() = 0;
     
+    /** Write the closing of an array value.
+     *  
+     *  \code
+     *  ]
+     *  \endcode
+    **/
     virtual void write_array_end() = 0;
     
+    /** Write the delimiter between two entries in an array.
+     *  
+     *  \code
+     *  ,
+     *  \endcode
+    **/
     virtual void write_array_delimiter() = 0;
     
+    /** Write a string value.
+     *  
+     *  \param value is the string to write. It will \e hopefully be encoded as valid UTF-8. It is the implementation's
+     *               choice of how to deal with malformed string values. Two common options are to replace malformed
+     *               sequences with ?s or to simply output these encodings and let the receiver deal with them.
+     *  
+     *  \code
+     *  "value"
+     *  \endcode
+    **/
     virtual void write_string(string_ref value) = 0;
     
+    /** Write an integer value.
+     *  
+     *  \code
+     *  902
+     *  \endcode
+    **/
     virtual void write_integer(std::int64_t value) = 0;
     
+    /** Write a decimal value.
+     *  
+     *  \param value is the decimal to write. Keep in mind that standard JSON does not support special IEEE 754 values
+     *               such as NaN and infinity. It is the implementation's choice of how to deal with such values. Two
+     *               common options are to output \c null or to encode a string description of the special value.
+     *  
+     *  \code
+     *  4.9
+     *  \endcode
+    **/
     virtual void write_decimal(double value) = 0;
     
+    /** Write a boolean value.
+     *  
+     *  \code
+     *  true
+     *  \endcode
+    **/
     virtual void write_boolean(bool value) = 0;
 };
 
+/** An encoder that outputs to an \c std::ostream. This implementation is used for \c operator<< on a \c value.
+**/
 class JSONV_PUBLIC ostream_encoder :
         public encoder
 {
 public:
+    /** Create an instance which places text into \a output. **/
     explicit ostream_encoder(std::ostream& output);
     
     virtual ~ostream_encoder() noexcept;
@@ -86,6 +169,7 @@ protected:
     
     virtual void write_integer(std::int64_t value) override;
     
+    /** When a special value is given, this will output \c null. **/
     virtual void write_decimal(double value) override;
     
     virtual void write_boolean(bool value) override;
