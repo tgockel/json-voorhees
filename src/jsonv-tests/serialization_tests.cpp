a6f7c25610be51016cca5e83d46797a24b6dda2b
Travis Gockel <travis@gockelhut.com>
2015-01-20 20:44:06 -0700

    Adds unit tests for member serialization of a class.

--- src/jsonv-tests/serialization_tests.cpp
+++ src/jsonv-tests/serialization_tests.cpp
@@ -14,6 +14,9 @@
 #include <jsonv/serialization.hpp>
 #include <jsonv/value.hpp>
 
+#include <tuple>
+#include <utility>
+
 namespace jsonv_test
 {
 
@@ -23,6 +26,41 @@ namespace
 {
 
 struct unassociated { };
+
+struct my_thing
+{
+    int a;
+    int b;
+    std::string c;
+    
+    my_thing(const value& from, const extraction_context& cxt) :
+            a(cxt.extract_sub<int>(from, "a")),
+            b(cxt.extract_sub<int>(from, "b")),
+            c(cxt.extract_sub<std::string>(from, "c"))
+    { }
+    
+    my_thing(int a, int b, std::string c) :
+            a(a),
+            b(b),
+            c(std::move(c))
+    { }
+    
+    static const extractor* get_extractor()
+    {
+        static extractor_construction<my_thing> instance;
+        return &instance;
+    }
+    
+    bool operator==(const my_thing& other) const
+    {
+        return std::tie(a, b, c) == std::tie(other.a, other.b, other.c);
+    }
+    
+    friend std::ostream& operator<<(std::ostream& os, const my_thing& self)
+    {
+        return os << "{ a=" << self.a << ", b=" << self.b << ", c=" << self.c << " }";
+    }
+};
 
 }
 
@@ -49,6 +87,25 @@ TEST(extract_basics)
     ensure_eq("thing", cxt.extract_sub<std::string>(val, "s"));
     ensure_throws(extraction_error, cxt.extract_sub<unassociated>(val, "o"));
     ensure_throws(extraction_error, cxt.extract_sub<int>(val, path::create(".a[3]")));
+}
+
+TEST(extract_object)
+{
+    formats fmts = formats::compose({ formats::defaults() });
+    fmts.register_extractor(my_thing::get_extractor());
+    
+    my_thing res = extract<my_thing>(parse(R"({ "a": 1, "b": 2, "c": "thing" })"), fmts);
+    ensure_eq(my_thing(1, 2, "thing"), res);
+}
+
+TEST(extract_object_search)
+{
+    formats base_fmts;
+    base_fmts.register_extractor(my_thing::get_extractor());
+    formats fmts = formats::compose({ formats::defaults(), base_fmts });
+    
+    my_thing res = extract<my_thing>(parse(R"({ "a": 1, "b": 2, "c": "thing" })"), fmts);
+    ensure_eq(my_thing(1, 2, "thing"), res);
 }
 
 }
