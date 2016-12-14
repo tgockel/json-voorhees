f702033e701ea64b310267ec90302fa65c0b3472
Travis Gockel <travis@gockelhut.com>
2016-12-13 17:10:20 -0700

    Benchmarking: Fixes compilation for C++11 (pre-movable std::_stream).

--- src/jsonv-tests/benchmark_tests.cpp
+++ src/jsonv-tests/benchmark_tests.cpp
@@ -27,13 +27,13 @@ using namespace jsonv;
 
 static const unsigned iterations = JSONV_DEBUG ? 1 : 100;
 
-template <typename FLoadSource>
-static void run_test(FLoadSource load, const std::string& from)
+template <typename THolster, typename FLoader>
+static void run_test(FLoader load, const std::string& from)
 {
     stopwatch timer;
     for (unsigned cnt = 0; cnt < iterations; ++cnt)
     {
-        auto src_data = load(from);
+        THolster src_data{load(from)};
         {
             JSONV_TEST_TIME(timer);
             parse(src_data);
@@ -42,24 +42,27 @@ static void run_test(FLoadSource load, const std::string& from)
     std::cout << timer.get();
 }
 
-template <typename FLoadSource>
+template <typename THolster>
 class benchmark_test :
         public unit_test
 {
 public:
-    benchmark_test(FLoadSource load_source, std::string source_desc, std::string path) :
+    using loader = std::string (*)(const std::string&);
+
+public:
+    benchmark_test(loader load, std::string source_desc, std::string path) :
             unit_test(std::string("benchmark/") + source_desc + "/" + filename(path)),
-            load_source(std::move(load_source)),
+            load(load),
             path(std::move(path))
     { }
 
     virtual void run_impl() override
     {
-        run_test(load_source, path);
+        run_test<THolster>(load, path);
     }
 
 private:
-    FLoadSource load_source;
+    loader      load;
     std::string path;
 };
 
@@ -68,16 +71,10 @@ class benchmark_test_initializer
 public:
     explicit benchmark_test_initializer(const std::string& rootpath)
     {
-        using ifstream_loader = std::ifstream (*)(const std::string&);
-        ifstream_loader ifstream_load = [] (const std::string& p) { return std::ifstream(p); };
-
-        using string_loader = std::string (*)(const std::string&);
-        string_loader string_load = load_from_file;
-
         recursive_directory_for_each(rootpath, ".json", [&, this] (const std::string& path)
         {
-            _tests.emplace_back(new benchmark_test<ifstream_loader>(ifstream_load, "ifstream", path));
-            _tests.emplace_back(new benchmark_test<string_loader>(string_load, "string", path));
+            _tests.emplace_back(new benchmark_test<std::ifstream>([] (const std::string& p) { return p; }, "ifstream", path));
+            _tests.emplace_back(new benchmark_test<std::string>(load_from_file, "string", path));
         });
     }
 
