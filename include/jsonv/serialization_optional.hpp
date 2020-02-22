54e6043951146cae32ef74724dd61621efab5d6c
Travis Gockel <travis@gockelhut.com>
2020-02-22 12:46:12 -0700

    Removes redundant implementations of C++17 classes.
    * `jsonv::detail::string_view` is completely removed
    * `jsonv::string_view` is now an alias for `std::string_view`
    * `jsonv::optional<T>` is now an alias for `std::optional<T>`
    
    Fixes issue #140.

--- include/jsonv/serialization_optional.hpp
+++ include/jsonv/serialization_optional.hpp
@@ -1,56 +1,49 @@
-/** \file jsonv/serialization_optional.hpp
-*  Template specialization to support optional<T> serialization.
-*  These are usually not needed unless you are writing your own
-*  \c extractor or \c serializer.
-*
-*  Copyright (c) 2015 by Travis Gockel. All rights reserved.
-*
-*  This program is free software: you can redistribute it and/or modify it under the terms of the Apache License
-*  as published by the Apache Software Foundation, either version 2 of the License, or (at your option) any later
-*  version.
-*
-*  \author Vladimir Venediktov (vvenedict@gmail.com)
-**/
-
-#ifndef __JSONV_SERIALIZATION_OPTIONAL_HPP_INCLUDED__
-#define __JSONV_SERIALIZATION_OPTIONAL_HPP_INCLUDED__
+/// \file jsonv/serialization_optional.hpp
+/// Template specialization to support optional<T> serialization. These are usually not needed unless you are writing
+/// your own \c extractor or \c serializer.
+///
+/// Copyright (c) 2015-2020 by Travis Gockel. All rights reserved.
+///
+/// This program is free software: you can redistribute it and/or modify it under the terms of the Apache License
+/// as published by the Apache Software Foundation, either version 2 of the License, or (at your option) any later
+/// version.
+///
+/// \author Vladimir Venediktov (vvenedict@gmail.com)
+#pragma once
 
 #include <jsonv/serialization_util.hpp>
 #include <jsonv/optional.hpp>
 
-
 namespace jsonv
 {
 
-//specialization for optional<T>
+/// \addtogroup Serialization
+/// \{
+
 template <typename T>
 class container_adapter<optional<T>> :
-    public adapter_for<optional<T>>
+        public adapter_for<optional<T>>
 {
     using element_type = optional<T>;
 
 protected:
     virtual optional<T> create(const extraction_context& context, const value& from) const override
     {
-        optional<T> out;
-        if (from.is_null()) {
-            return out;
-        }
-        out = context.extract<T>(from);
-        return out;
+        if (from.is_null())
+            return nullopt;
+        else
+            return context.extract<T>(from);
     }
 
     virtual value to_json(const serialization_context& context, const optional<T>& from) const override
     {
-        value out;
-        if (from) {
-            out = context.to_json(*from);
-        }
-        return out;
+        if (from)
+            return context.to_json(*from);
+        else
+            return null;
     }
 };
 
+/// \}
+
 }
-
-#endif
-
--- include/jsonv/string_view.hpp
+++ include/jsonv/string_view.hpp
@@ -1,56 +1,23 @@
-/** \file jsonv/string_view.hpp
- *  Pulls in an implementation of \c string_view.
- *  
- *  Copyright (c) 2014 by Travis Gockel. All rights reserved.
- *
- *  This program is free software: you can redistribute it and/or modify it under the terms of the Apache License
- *  as published by the Apache Software Foundation, either version 2 of the License, or (at your option) any later
- *  version.
- *
- *  \author Travis Gockel (travis@gockelhut.com)
-**/
-#ifndef __JSONV_STRING_VIEW_HPP_INCLUDED__
-#define __JSONV_STRING_VIEW_HPP_INCLUDED__
+/// \file jsonv/string_view.hpp
+/// Pulls in an implementation of \c string_view.
+///
+/// Copyright (c) 2014-2020 by Travis Gockel. All rights reserved.
+///
+/// This program is free software: you can redistribute it and/or modify it under the terms of the Apache License
+/// as published by the Apache Software Foundation, either version 2 of the License, or (at your option) any later
+/// version.
+///
+/// \author Travis Gockel (travis@gockelhut.com)
+#pragma once
 
 #include <jsonv/config.hpp>
 
-/** \def JSONV_STRING_VIEW_TYPE
- *  The type to use for \c jsonv::string_view. By default, this is \c jsonv::detail::string_view.
- *  
- *  \def JSONV_STRING_VIEW_INCLUDE
- *  The file to include to get the implementation for \c string_view. If you define \c JSONV_STRING_VIEW_TYPE, you must
- *  also define this.
- *  
- *  \def JSONV_STRING_VIEW_USE_STD
- *  Set this to 1 to use \c std::string_view as the backing type for \c jsonv::string_view.
- *  
- *  \def JSONV_STRING_VIEW_USE_BOOST
- *  Set this to 1 to use \c boost::string_ref as the backing type for \c jsonv::string_view.
-**/
-#ifndef JSONV_STRING_VIEW_TYPE
-#   if defined(JSONV_STRING_VIEW_USE_STD) && JSONV_STRING_VIEW_USE_STD
-#       define JSONV_STRING_VIEW_TYPE    std::string_view
-#       define JSONV_STRING_VIEW_INCLUDE <string_view>
-#   elif defined(JSONV_STRING_VIEW_USE_BOOST) && JSONV_STRING_VIEW_USE_BOOST
-#       define JSONV_STRING_VIEW_TYPE    boost::string_ref
-#       define JSONV_STRING_VIEW_INCLUDE <boost/utility/string_ref.hpp>
-#   else
-#       define JSONV_STRING_VIEW_TYPE    jsonv::detail::string_view
-#       define JSONV_STRING_VIEW_INCLUDE <jsonv/detail/string_view.hpp>
-#   endif
-#endif
-
-#include JSONV_STRING_VIEW_INCLUDE
+#include <string_view>
 
 namespace jsonv
 {
 
-/** A non-owning reference to a string.
- *  
- *  \see http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2012/n3442.html
-**/
-using string_view = JSONV_STRING_VIEW_TYPE;
+/// A non-owning reference to a string.
+using string_view = std::string_view;
 
 }
-
-#endif/*__JSONV_STRING_REF_HPP_INCLUDED__*/
